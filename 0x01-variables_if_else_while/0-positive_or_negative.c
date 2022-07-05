63b65f30970a3e0f885df994fbbc52a607ee099f
Anyamba Chukunzira <achuku@gmail.com>
2022-07-05 08:32:45 -0700

    Positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
 {
-printf(n" is positive\n");
+printf("%d is positive\n");
 }
-if (n == 0)
+else if (n == 0)
 {
-printf(n" is zero\n");
+printf("%d is zero\n");
 }
-if (n < 0)
+else (n < 0)
 {
-printf(n "is negative\n");
+printf("%d is negative\n");
 }
 return (0);
 }
