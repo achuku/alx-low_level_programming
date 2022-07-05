628d3c70914b3744f1d3191908715d8e089fd39b
Anyamba Chukunzira <achuku@gmail.com>
2022-07-05 02:01:04 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /*
- * main: the main function to be executed
+ * main: This is the main function to be executed
  * return: always 0 (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
