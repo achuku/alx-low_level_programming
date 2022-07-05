bb719cb7103d6daf88321275f426f7d9da1961e1
Anyamba Chukunzira <achuku@gmail.com>
2022-07-05 01:39:18 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 /**
- * This is the main function that will be executed when the program is run
- * Return should be zero if successful
- */
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
