5d1d311b6b039e073a326010c270ab661510260c
Anyamba Chukunzira <achuku@gmail.com>
2022-07-05 02:29:45 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main is the executable function
+ * return 0 means success
+ */
+int main(void)
+{
+char chartype;
+int inttype;
+long int longinttype;
+long long int llonginttype;
+float floattype;
+
+printf("Size of a char: %u byte(s)\n", sizeof(chartype));
+printf("Size of an int: %u byte(s)\n", sizeof(inttype));
+printf("Size of a long int: %u byte(s)\n", sizeof(longinttype)); 
+printf("Size of a long long int: %u byte(s)\n", sizeof(llonginttype)); 
+printf("Size of a float: %u byte(s)\n", sizeof(floattype)); 
+
+return (0);
+}
