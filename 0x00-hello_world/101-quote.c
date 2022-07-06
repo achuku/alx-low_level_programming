fea26fedfc756f1f14f5fbc17653af0e6eae1f3e
Anyamba Chukunzira <achuku@gmail.com>
2022-07-06 01:58:18 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include<unistd.h>
 /**
  * main - ENtry point
  * return: Always 1 (Success)
  */
 int main(void)
 {
-char stri[] = "and that piece of art is useful\" - \ Dora Korpar, 2015-10-19\n";
+char stri[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, stri, 59);
 return (1);
 }
